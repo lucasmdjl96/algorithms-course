aaa7da39cef61d44709fde3f0dda50c0c44aa115
lucasmdjl96 <lucasmdjl96@gmail.com>
2022-12-12 21:00:42 +0100

    solve: introduction problem part i

---
+++ Part I/hello/src/main/java/RandomWord.java
@@ -0,0 +1,15 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String champion = null;
+        int i = 0;
+        while (!StdIn.isEmpty()) {
+            i++;
+            if (StdRandom.bernoulli(1.0/i)) champion = StdIn.readString();
+        }
+        StdOut.println(champion);
+    }
+}
